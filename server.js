e1cb9e72ff94dbd73dafc9172242f2cf401b0715
John Cody Nemmer <jonemmer@hotmail.com>
2016-05-23 13:19:11 -0500

    Setting Up Server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+//Express
 var express = require('express');
+var app = express();
+
+//Body Parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//Static Folder
+app.use(express.static(__dirname + '/client'));
+
+//Mongoose
+require('./server/config/mongoose');
+
+//Routes
+//require('./server/config/routes')(app);
+
+//Port Location
+app.listen(3500, function(){
+	console.log('Server listening on port: 3500')
+})
---
+++ server/config/mongoose.js
@@ -0,0 +1,14 @@
+//Set Up For Mongoose
+var mongoose = require('mongoose');
+
+var fs = require('fs');
+
+mongoose.connect('mongodb://localhost/OrdersCustomers');
+
+var models_path = __dirname + "/../models";
+
+fs.readdirSync(models_path).forEach(function(file){
+	if(file.indexOf('.js') > 0){
+		require(models_path + '/' + file);
+	}
+})
