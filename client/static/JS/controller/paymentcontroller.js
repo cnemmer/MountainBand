f5b505b78d67f3c2d69b085637499cff42a7f868
John Cody Nemmer <jonemmer@hotmail.com>
2016-05-26 18:57:17 -0500

    No real changes, getting ready to pull latest

--- client/static/JS/controller/paymentcontroller.js
+++ client/static/JS/controller/paymentcontroller.js
@@ -1,3 +1,10 @@
 myApp.controller('PaymentController',  function(CheckoutFactory){
+	var self = this;
+
 	this.clientSubmittedInfo = CheckoutFactory.clientSubmittedInfo;
+	//console.log(this.clientSubmittedInfo)
+
+	/*this.remove = function(clientId){
+
+	}*/
 })
